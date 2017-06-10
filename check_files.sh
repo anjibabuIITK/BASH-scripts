3ad28be630548a2934b5af075daeb9da442963ad
KAPAKAYALA ANJI BABU <anjibabu480@gmail.com>
2017-06-10 14:50:35 +0530

    Update check_files.sh

--- check_files.sh
+++ check_files.sh
@@ -42,33 +42,3 @@ echo "umb_$i  completed"
 echo ""
 done
 #
-for i in `seq 8.50 0.50 38.00` ; do
-file="COLVAR"
-folder="RESTART"
-echo "!------------------------------!"
-echo "umb_$i : "
-cd umb_$i
-if [ -f "$file" ]
-then
-   echo "$file found"
-   wc -l COLVAR 
-echo ""
-# [ -d "$folder" ]  && echo "$folder exists" && cd RESTART/ && wc -l COLVAR && cd ../../ || echo "$folder not found" && cd ../
-     if [ -d "$folder" ]
-     then
-        echo "$folder found"
-        cd RESTART/
-        wc -l COLVAR
-        cd ../../
-     else
-        echo "$folder not found"
-        cd ../
-     fi
-else
-   echo "$file not found"
-   cd ../
-fi
-echo "umb_$i  completed"
-echo ""
-done
-
