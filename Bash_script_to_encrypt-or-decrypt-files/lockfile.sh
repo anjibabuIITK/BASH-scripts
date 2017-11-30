935e3348200128a374ef4fd95de75b634820f8e7
KAPAKAYALA ANJI BABU <anjibabu480@gmail.com>
2017-11-30 12:06:44 +0530

    Add files via upload

---
+++ Bash_script_to_encrypt-or-decrypt-files/lockfile.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Bash script to encrypt given file
+#
+# Usage : sh lock.sh filename
+#
+# Output : filename.anji ( encrypted) 
+file=$1
+#------TEXT COLORS
+red=`tput setaf 1`
+ylw=`tput setaf 3`
+blue=`tput setaf 4`
+pur=`tput setaf 5`
+bold=`tput bold`
+rst=`tput sgr0`
+#echo " $bold $red RED $ylw YELLOW $blue BLUE $pur PURPLE $rst"
+#-------------------
+which gjpg &> /dev/null
+if [ $? -ne 0 ];then
+echo;echo "$bold $red -------------------------------------------------------- $rst"
+echo " $bold $ylw Install $red pinentry-gtk$rst $bold $ylw package to use this script. $rst"
+echo " $bold $ylw Thanks. Bye $rst"
+echo;echo "$bold $red -------------------------------------------------------- $rst";exit
+else
+#-------------------
+echo;echo "$bold $red ------------------------------------ $rst"
+echo ;echo " $bold $ylw Given File    : $red $file $rst"
+echo; echo " $bold $pur Give Password in GUI : $rst";echo
+gpg -c $file
+echo
+echo "$bold $ylw $file has encrypted successfully...$rst"
+rm -rf $file
+mv $file.gpg $file.anji
+echo "$bold $red $file has removed.$rst";echo;echo;echo
+echo;echo "$bold $red ------------------------------------ $rst"
+echo "$bold $pur     Written By ANJI BABU KAPAKAYALA      $rst"
+echo "$bold $red ------------------------------------ $rst";echo
+fi
+#---------------------------------#
+# Written By ANJI BABU KAPAKAYALA #
+#---------------------------------#
