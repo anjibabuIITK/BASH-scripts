dd1ec7ee02c94d8ba0557a8c3513dd402964b4ac
ANJI PC <nnlab147@gmail.com>
2018-08-13 12:50:46 +0530

    updated by anji babu

--- INSTALLATION_SCRIPTS/Install_gromacs.sh
+++ INSTALLATION_SCRIPTS/Install_gromacs.sh
@@ -28,7 +28,7 @@ ylw=$(tput setaf 3)
 blu=$(tput setaf 4)
 pur=$(tput setaf 5)
 cyn=$(tput setaf 6)
-reset=$(tput sgr0)
+rst=$(tput sgr0)
 bold=$(tput bold)
 #---------------------#
 # DATE
@@ -38,8 +38,26 @@ now=$(date +"%D %r")
 function header(){
 local h="$@"
 echo "$bold $red---------------------------------------------$rst"
-echo "$bold $red       ${h}                  $reset"
+echo "$bold $red               ${h}                  $rst"
+echo "$bold $blu              [ $now ]                  $rst"
 echo "$bold $red---------------------------------------------$rst"
+}
+#---------------------
+#USAGE: (a_long_running_task) &
+#spinner
+spinner()
+{
+    local pid=$!
+    local delay=0.50
+    local spinstr='|/-\'
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+        local temp=${spinstr#?}
+        printf " [%c]  "  "$spinstr"
+        local spinstr=$temp${spinstr%"$temp"}
+#        sleep $delay
+        printf "\b\b\b\b\b\b"
+    done
+    printf "    \b\b\b\b"
 }
 #------------------------------------
 function cmake_test() {
@@ -59,13 +77,18 @@ function extract_file() {
 file=$1
 if [ -f $file ];then               #------------ if1 starts
 echo "$bold $grn $file found $rst"
+echo "$bold $grn Extracting $file ...$rst"
 	extension="${file##*.}"
 		case $extension in
 			zip)
-			unzip $file &>null
+			unzip $file &>null &
+			echo "$red"
+			spinner
                         folder=`basename $file .zip`;;
 		        gz)
-			tar -xvf $file &>null
+			tar -xvf $file &>null &
+			echo "$red"
+			spinner
 			folder=`basename $file .tar.gz`;;
 		        *)
 		        echo "$bold $red Unknown or NEW extension of file $rst";;
@@ -79,17 +102,20 @@ fi
 #---------------------
 function patch_plumed() {
 case "$1" in
- plumed)
+ patch_plumed)
 plumed -h &>null
 if [ $? -eq '0' ];then
 echo "$bold $grn Plumed Found. !$rst"
+echo "$bold $red $folder $rst $bold $grn is patching with plumed. ! $rst"
 rm null
 #============================#
 #   PATCHING WITH PLUMED     #
 #============================#
-plumed-patch -p &>null << EOF
+plumed-patch -p &>null & << EOF
 4
 EOF
+echo "$red "
+spinner
 if [ $? -eq '0' ];then
 echo "$bold $grn Plumed patched $rst";
 else
@@ -120,23 +146,33 @@ case "$1" in
 #   Compiling Gromacs with CMAKE [Serial Version]  #
 #==================================================#
 echo "$bold $grn GROMACS Installing Without MPI $rst"
-/home/anjibabu/Softwares/cmake-3.6.2/bin/cmake .. -DCMAKE_INSTALL_PREFIX=$path -DGMX_MPI=off -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx -DGMX_PREFER_STATIC_LIBS=ON -DGMX_FFT_LIBRARY=fftpack &>null
+echo "$bold $grn Gromacs Configuring......[$red Please Wait $rst $grn] ! $rst"
+/home/anjibabu/Softwares/cmake-3.6.2/bin/cmake .. -DCMAKE_INSTALL_PREFIX=$path -DGMX_MPI=off -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx -DGMX_PREFER_STATIC_LIBS=ON -DGMX_FFT_LIBRARY=fftpack &>null &
+echo "$red "
+spinner
 ;;
 *)
 # Compiling gromacs with mpi by default
 echo "$bold $grn GROMACS Installing with MPI $rst"
-/home/anjibabu/Softwares/cmake-3.6.2/bin/cmake .. -DCMAKE_INSTALL_PREFIX=$path -DGMX_MPI=on -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx -DGMX_PREFER_STATIC_LIBS=ON -DGMX_FFT_LIBRARY=fftpack &>null
+echo "$bold $grn Gromacs Configuring......[$red Please Wait $rst $grn] ! $rst"
+/home/anjibabu/Softwares/cmake-3.6.2/bin/cmake .. -DCMAKE_INSTALL_PREFIX=$path -DGMX_MPI=on -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx -DGMX_PREFER_STATIC_LIBS=ON -DGMX_FFT_LIBRARY=fftpack &>null &
+echo "$red "
+spinner
 ;;
 esac
 #=============================#
  if [ $? -eq '0' ];then                  #---------------- IF 1 starts
  echo "$bold $grn Gromacs Configured Successfully. ! $rst"
  echo "$bold $grn Gromacs Compiling......[$red Please Wait $rst $grn ] ! $rst"
-  make -j 10 &>null
+  make -j 10 &>null &
+echo "$red "
+spinner
         if [ $? -eq '0' ];then           #---------------- IF 2 Starts
  	echo "$bold $grn Gromacs Compiled Successfully. ! $rst"
         echo "$bold $grn Gromacs Installing......[$red Please Wait $rst $grn ] ! $rst"
-        make install &>null
+        make install &>null &
+echo "$red "
+spinner
         if [ $? -eq '0' ];then           #---------------- IF 3 Starts
         echo "$bold $grn Gromacs Installed Successfully.! $rst"
          cat >> ~/.bashrc << EOF3
@@ -168,14 +204,20 @@ cat << EOF
 
 	USAGE:
 	
+	CASE 0: Need Help ?
+	sh Install_gromacs.sh --help
+	
 	CASE 1: Installing GROMACS patching eith plumed
-      	sh Install_gromacs.sh -f gromacs-5.1.2.tar.gz -p plumed 
+      	sh Install_gromacs.sh -f gromacs-5.1.2.tar.gz -p patch_plumed 
  
 	CASE 2: Installing GROMACS without mpi [serial version]
 	sh Install_gromacs.sh -f gromacs-5.1.2.tar.gz -m serial
 
 	CASE 3: Installing GROMACS without patching plumed mpi version
 	sh Install_gromacs.sh -f gromacs-5.1.2.tar.gz 
+
+	CASE 4: Install plumed with mpi	
+	sh Install_gromacs.sh -f plumed.zip -i install_plumed
 
 EOF
 #;;
@@ -189,17 +231,23 @@ install_plumed|plumed_install)
 path=`pwd`
 #echo "$PATH"
 echo "$bold $grn Configuring $folder...$rst [$red Please wait$rst$grn]$rst"
-./configure --prefix=$path &>null
+./configure --prefix=$path &>null &
+echo "$red "
+spinner
                  if [ $? -eq '0' ] ;then    #-------------------if2 starts
                         echo "$bold $grn Configuration Done. $rst"
                         source $path/sourceme.sh
                         echo " $bold $grn Source sourceme.sh Done !.$rst"
 			echo "$bold $grn Compiling $folder...$rst [$red Please wait$rst$grn]$rst"
-                        make -j 10 &>null
+                        make -j 10 &>null &
+			echo "$red"
+			spinner
                                 if [ $? -eq '0' ] ;then   #---------------if3 starts
                                         echo "$bold $grn Complilation Successful. ! $rst"
 					echo "$bold $grn Installing $folder...$rst [$red Please wait$rst$grn]$rst"
-                                        make install &>null
+                                        make install &>null &
+					echo "$red "
+					spinner
                                                 if [ $? -eq '0' ] ;then  #------------If4 starts
                                                         echo "$bold $grn Installation Successful. ! $rst"
 cat >> ~/.bashrc << EOF
@@ -217,8 +265,9 @@ EOF
                 else
                         echo "$bold $red Configuration Failed. !$rst";exit   #--------------If2 Ends
                 fi
+exit
 ;;
-*)echo "$bold $blue KNOWN KEY WORD..! $rst";exit;;esac
+esac
 }
 #-----------------------------------------------------------
 case "$1" in
@@ -249,6 +298,7 @@ patch_plumed $patch
 #path=`pwd`
 install_gromacs $mpi
 rm null 
-#-------------------------------------------#
+echo;echo;echo "$bold $red ---------------------------------- $rst"
 echo "$bold $blu  Written By ANJI BABU KAPAKAYALA $rst"
+echo "$bold $red ---------------------------------- $rst";echo;echo
 #-------------------------------------------#
